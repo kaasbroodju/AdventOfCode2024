8c42b77c6d59c97684281d512027e6082cb3c0b0
kaasbroodju <morriswaaijer8@gmail.com>
2024-12-04 02:23:12 +0100

    Refactor day_01 part 1

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,20 +1,27 @@
+use std::ops::Sub;
+
 pub fn first_part() {
-    let mut left_side = vec![];
-    let mut right_side = vec![];
+    let mut left_side: Vec<usize> = vec![];
+    let mut right_side: Vec<usize> = vec![];
 
-    include_str!("input.txt").lines().for_each(|line| {
-        let (l, r) = line.split_once("   ").unwrap();
-        left_side.push(l.parse::<i32>().unwrap());
-        right_side.push(r.parse::<i32>().unwrap());
-    });
+    include_str!("input.txt")
+        .lines()
+        .map(|line| line.as_bytes())
+        .for_each(|line| {
+            let left_number = (line[0] - b'0') as usize * 10000 + (line[1] - b'0') as usize * 1000 + (line[2] - b'0') as usize * 100 + (line[3] - b'0') as usize * 10 + (line[4] - b'0') as usize;
+            let idx = left_side.partition_point(|&x| x <= left_number);
+            left_side.insert(idx, left_number);
 
-    left_side.sort();
-    right_side.sort();
+            let right_number = (line[8] - b'0') as usize * 10000 + (line[9] - b'0') as usize * 1000 + (line[10] - b'0') as usize * 100 + (line[11] - b'0') as usize * 10 + (line[12] - b'0') as usize;
+            let idx = right_side.partition_point(|&x| x <= right_number);
+            right_side.insert(idx, right_number);
+        });
+
     let delta = left_side
         .iter()
         .zip(right_side.iter())
-        .map(|(l, r)| (r - l).abs())
-        .sum::<i32>();
+        .map(|(&l, &r)| l.abs_diff(r))
+        .sum::<usize>();
 
     let mut delta_score = vec![];
     let mut i = 0;
@@ -28,13 +35,13 @@ pub fn first_part() {
                 counter += 1;
                 i += 1;
             }
-            delta_score.push((l * counter).abs());
+            delta_score.push((l * counter));
         }
     }
 
     println!("part one: {}", delta);
     println!("part two: {:?}", delta_score);
-    println!("part two: {:?}", delta_score.iter().sum::<i32>());
+    println!("part two: {:?}", delta_score.iter().sum::<usize>());
 
     // println!("{:?}", left_side);
     // println!("{:?}", right_side)
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod day_02;
 mod day_03;
 
 fn main() {
-    // day_03::first_part();
+    day_01::first_part();
     // day_03::first_part_state_machine();
     day_03::second_part();
     day_03::second_part_state_machine();
